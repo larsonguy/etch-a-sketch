0c14d2dcb6e86d446dbb18bcb092b1ab89e7fdf7
Your Name <lbellomo@flatiron.com>
2022-10-24 23:53:24 -0400

    Add clear grid functionality + random RGB for each event listener.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-
-let gridStatus = '';
+function getRandomRGB() {
+    color = Math.floor(Math.random() * 256);
+    return color;
+}
 
 function createGrid(userSize) {
-    const container = document.querySelector('#container')
+    const grid = document.querySelector('#grid');
+    let container = document.createElement('div');
+    container.setAttribute('id', 'container');
     container.setAttribute('style', `grid-template-columns: repeat(${userSize}, 1fr); grid-template-rows: repeat(${userSize}, 1fr);`);
+    grid.appendChild(container);
     for (i = 0; i < (userSize * userSize); i++) {
         let gridLW = (960/userSize);
         let gridDiv = document.createElement('div');
-        gridDiv.className = 'grid';
+        gridDiv.setAttribute('class', 'grid');
         gridDiv.setAttribute('style', `width: ${gridLW}; height: ${gridLW};`)
         container.appendChild(gridDiv);
     }
@@ -15,24 +20,28 @@ function createGrid(userSize) {
     const gridDiv = document.querySelectorAll('.grid');
     gridDiv.forEach(div => {
             div.addEventListener('mouseenter', function(event) {
-            event.target.style.backgroundColor = "purple";
+            event.target.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
         }, false);
     })
-    let gridStatus = 'active'
-}
-
-function clearGrid() {
-
+    
 }
 
 function setSize(){
-    if (gridStatus === 'active'){
-        clearGrid();
+    var element =  document.getElementById('grid');
+    if (typeof(element) != 'undefined' && element != null) {
+        element.innerHTML = '';
+        let userInput = prompt("What size would you like for this etch-a-sketch? (e.g., '16' equals 16x16)")
+        if(parseInt(userInput) > 100) {
+            alert('Please do not select a number greater than 100. Try again.')
+            return
+        }
+        createGrid(userInput);
     } else {
-    let userInput = prompt("What size would you like for this etch-a-sketch? (e.g., '16' equals 16x16)")
-    createGrid(userInput);
+        let userInput = prompt("What size would you like for this etch-a-sketch? (e.g., '16' equals 16x16)")
+        createGrid(userInput);
     }
-} 
+}
+
 
 
 
